5006cbfc495a647a75d94bd68a5089ef9bcf39a6
Arnas Daraška PS25 <arnas.daraska@stud.svako.lt>
2026-03-12 12:20:04 +0200

    Pirmas Flask projekto ikelimas

---
+++ app.py
@@ -0,0 +1,27 @@
+# app.py - pagrindinė Flask aplokacijos logika
+# ŠIAME FAILE APRAŠYTI MARŠRUTAI (routes)
+
+from flask import Flask, render_template
+
+# sukuriame flask aplikacijos objektą
+app = Flask(__name__)
+
+# Maršrutas: pagrindinis puslapis
+# URL:/
+# Metodas: GET
+# atvaizduoja index.html šabloną
+@app.route("/") # Dekoratorius
+def index():
+    return render_template("index.html")
+
+# Maršrutas: kontaktų puslapis (forma)
+# Metodai: GET ir POST
+@app.route("/kontaktai")
+def kontaktai():
+    return render_template("kontaktai.html")    
+
+# aplikacijos paleidimas
+# paleidimo metu kai debbug = True vyksta automatinis aplikacijos perkrovimas pakeitus kodą plius klaidų rodymas
+if __name__ == "__main__":
+    app.run(debug = True)
+
